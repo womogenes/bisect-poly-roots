ad8dc042f6b6a44327a8ab7884b22b11964b67b7
William Y. Feng <williamfeng1729@gmail.com>
2021-11-02 18:06:36 -0700

    Fix a whole bunch of bugs

--- bisect_poly_roots.py
+++ bisect_poly_roots.py
@@ -1,4 +1,7 @@
-﻿def sign(x):
+﻿from numpy import TooHardError
+
+
+def sign(x):
     if x > 0:
         return 1
     if x < 0:
@@ -32,7 +35,7 @@ def peval(poly, x):
     """
     ans = 0
     for i in range(len(poly)):
-        #print(x, i)
+        # print(x, i)
         ans += poly[i] * pow(x, i)
     return ans
 
@@ -40,7 +43,7 @@ def peval(poly, x):
 def roots(poly, error=0.0001):
     """
     poly: list of integers. poly[i] is coefficient of x^i.
-    Find the roots of poly.
+    Returns the roots of poly as a list.
     """
     # Remove 0s from the end
     if poly[-1] == 0:
@@ -56,15 +59,16 @@ def roots(poly, error=0.0001):
 
     # For non-linear, we use the fancy process
     deriv = derivative(poly)
-    d_roots = roots(deriv, error=error)
+    d_roots = sorted(roots(deriv, error=error))
 
-    ans = []
+    ans = set()
 
     """
+    print()
     print("poly:", poly)
     print("deriv:", deriv)
+    print("d_str:", poly_to_str(deriv))
     print("d_roots:", d_roots)
-    print()
     """
 
     # What if the derivative has no roots?
@@ -73,7 +77,7 @@ def roots(poly, error=0.0001):
         y_int = peval(poly, 0)
 
         if abs(y_int) <= error:
-            return [y_int]
+            return [0]
 
         if sign(poly[-1]) == sign(y_int):
             # Look to the left
@@ -89,23 +93,24 @@ def roots(poly, error=0.0001):
                 hi *= 2
 
         # Now we have our bounds
+        f_lo = peval(poly, lo)
         while hi - lo > error:
             mid = (lo + hi) / 2
             f_mid = peval(poly, mid)
             if abs(f_mid) < error:
-                return [f_mid]
-            if sign(f_mid) != sign(peval(poly, lo)):
+                return [mid]
+            if sign(f_mid) != sign(f_lo):
                 hi = mid
             else:
                 lo = mid
+                f_lo = peval(poly, lo)
 
         return [(lo + hi) / 2]
 
     # Analyze left endpoint
     f_left = peval(poly, d_roots[0])
-
     if abs(f_left) < error:
-        ans.append(f_left)
+        ans.add(d_roots[0])
     else:
         neg_end_behavior = pow(-1, deg % 2) * sign(poly[-1])
         if neg_end_behavior != sign(f_left):
@@ -125,12 +130,12 @@ def roots(poly, error=0.0001):
                     hi = mid
                 else:
                     lo = mid
-            ans.append((lo + hi) / 2)
+            ans.add((lo + hi) / 2)
 
     # Analyize right endpoint (this can probably be made less redundant)
     f_right = peval(poly, d_roots[-1])
-    if f_right == 0:
-        ans.append(f_right)
+    if abs(f_right) < error:
+        ans.add(d_roots[-1])
     else:
         pos_end_behavior = sign(poly[-1])
         if pos_end_behavior != sign(f_right):
@@ -150,22 +155,22 @@ def roots(poly, error=0.0001):
                     hi = mid
                 else:
                     lo = mid
-            ans.append((lo + hi) / 2)
+            ans.add((lo + hi) / 2)
 
     # Do stuff in between the roots
     for i in range(0, len(d_roots) - 1):
         lo = d_roots[i]
         hi = d_roots[i + 1]
 
-        if abs(lo) <= error:
-            ans.append(lo)
-            continue
-        if abs(hi) <= error:
-            ans.append(hi)
-            continue
-
         f_lo = peval(poly, lo)
         f_hi = peval(poly, hi)
+
+        if abs(f_lo) <= error:
+            ans.add(lo)
+            continue
+        if abs(f_hi) <= error:
+            ans.add(hi)
+            continue
 
         if sign(f_lo) == sign(f_hi):
             continue
@@ -183,15 +188,17 @@ def roots(poly, error=0.0001):
                 lo = mid
                 f_lo = peval(poly, lo)
 
-        ans.append((lo + hi) / 2)
+        ans.add((lo + hi) / 2)
 
-    return ans
+    return list(ans)
 
 
 # Test stuff out
 if __name__ == "__main__":
-    P = [1, 1, 3, 3]
+    P = [0, 2, -8, -4, -4, 7, 1, 3, -8, -3, -6]
     r = roots(P, error=0.00000001)
 
+    print()
     print(poly_to_str(P))
+    print(P)
     print(r)
--- test.py
+++ test.py
@@ -2,36 +2,58 @@
 import numpy as np
 from numpy.polynomial.polynomial import Polynomial
 from random import randrange
-import timeit
+import time
 
 from bisect_poly_roots import roots, poly_to_str
 
 TEST_ERROR = 0.01
 N = 1000
+DEGREE = 10
 
 
 def test_one(deg=100):
     while True:
-        poly = np.random.randint(-10, 10, size=(deg + 1))
+        poly = list(np.random.randint(-10, 10, size=(deg + 1)))
         if poly[-1] != 0:
             break
 
     try:
-        test_roots = roots(poly)
+        test_roots = roots(poly, error=0.00000001)
     except AssertionError as e:
         print(f"AssertionError for {poly}")
         return False
 
     np_poly = Polynomial(poly)
-    real_roots = [x.real for x in np_poly.roots() if abs(x.imag) < 0.0000001]
+    real_roots = list(
+        set([x.real for x in np_poly.roots() if abs(x.imag) < 0.0000001]))
 
     if len(test_roots) != len(real_roots):
         print(f"Test failed for {poly}")
+        print("Test roots:", test_roots)
+        print("Real roots:", real_roots)
         return False
+
+    test_roots.sort()
+    real_roots.sort()
+
+    for i in range(len(test_roots)):
+        if abs(real_roots[i] - test_roots[i]) > TEST_ERROR:
+            print(f"Test failed for {poly}")
+            print("Test roots:", test_roots)
+            print("Real roots:", real_roots)
+            return False
 
     return True
 
 
+start_time = time.time()
+passed = True
 for _ in range(N):
-    if not test_one(3):
+    if not test_one(DEGREE):
+        passed = False
         break
+
+if passed:
+    elapsed = time.time() - start_time
+    print(
+        f"{N} tests passed in {round(elapsed, 2)} seconds (average of {round(elapsed / N, 2)} sec per run).")
